5667e7cfecd2b99aef4d5ce5b19585f91b9c2823
Sita Bérété <sita.berete.3@gmail.com>
2022-08-03 23:52:17 +0000

    Create Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import Navbar from "./components/Navbar";
+import './App.css'
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react";
+import { Avatar, Menu, Typography } from "antd";
+import { Link } from "react-router-dom";
+import { ItemType } from "antd/lib/menu/hooks/useItems";
+import CryptoIcon from "../images/cryptocurrency.png";
+import {
+  HomeOutlined,
+  FundOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+} from "@ant-design/icons";
+
+const menuItems: ItemType[] = [
+  { label: <Link to="/">Home</Link>, icon: <HomeOutlined />, key: "home" },
+  {
+    label: <Link to="/cryptocurrencies">Cryptocurrencies</Link>,
+    icon: <FundOutlined />,
+    key: "cryptocurrencies",
+  },
+  {
+    label: <Link to="/exchanges">Exchanges</Link>,
+    icon: <MoneyCollectOutlined />,
+    key: "exchanges",
+  },
+  { label: <Link to="/news">news</Link>, icon: <BulbOutlined />, key: "news" },
+];
+
+const Navbar: FC = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Typography.Title level={2} className="logo">
+          <Link to="/">
+            <Avatar src={CryptoIcon} size="large" />
+            <span className='logo-text'>Crypto</span>
+          </Link>
+        </Typography.Title>
+      </div>
+      <Menu theme="dark" items={menuItems} />
+    </div>
+  );
+};
+
+export default Navbar;
